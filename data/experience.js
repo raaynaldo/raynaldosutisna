546512ab4e2ffd42fb2e1e0eed042b4f3045fc61
raaynaldo <raaynaldo@hotmail.com>
2021-06-01 23:50:37 -0400

    feat: insert experience data

---
+++ data/experience.js
@@ -0,0 +1,58 @@
+const experiences = [
+  {
+    id: 1,
+    companyName: 'VitusVet',
+    companyUrl: 'https://vitusvet.com/',
+    title: 'Software Engineer',
+    timeline: 'June 2021 - Present',
+    descriptions: ['hello', 'hello again', 'yeay'],
+  },
+  {
+    id: 2,
+    companyName: 'Code the Dream',
+    companyUrl: 'https://codethedream.org/',
+    title: 'Volunteer Mentor',
+    timeline: 'April 2021 - Present',
+    descriptions: [
+      'Guiding students to help with their assignments.',
+      'Teaching about the basics of programming with Javascript.',
+      'Sharing resources to support their learning process.',
+    ],
+  },
+  {
+    id: 3,
+    companyName: 'Emerio Indonesia',
+    companyUrl: 'https://www.emeriocorp.com/',
+    title: 'Full Stack Web Developer',
+    timeline: 'November 2018 - August 2019',
+    descriptions: [
+      'Was part of the implementation team of the Regla IFRS 9 system for two of the top five banks in Indonesia.',
+      'Designed and built user interface IFRS9 application requirements using ASP.NET Web Form.',
+      'Involved in software architecture design by leveraging the most efficient and effective frameworks.',
+      'Maintained version control system (git) and mentored an internship student to understand project structure.',
+    ],
+  },
+  {
+    id: 4,
+    companyName: 'Mitra Integrasi Informatika',
+    companyUrl: 'https://www.mii.co.id/',
+    title: 'Full Stack Web Developer',
+    timeline: 'October 2017 - September 2018',
+    descriptions: [
+      'Collaborated with system analysts to analyze customer requirements and design systems.',
+      'Developed three custom system applications using ASP.NET MVC.',
+      'Led the development team and created project milestones with a system analyst to determine main days.',
+      'Deployed system on client environment and involved the user acceptance test phase.',
+    ],
+  },
+  {
+    id: 5,
+    companyName: 'Neurosoft Indonesia',
+    title: 'Full Stack Web Developer',
+    timeline: 'May 2015 - February 2017',
+    descriptions: [
+      'Developed and maintained Head Quarter, which is Neurosoft’s integrated point of sales system using ASP.NET.',
+      'Developed Sales and Vehicle module for Honda ERP System (NeuroMotor) using ASP.NET MVC.',
+    ],
+  },
+];
