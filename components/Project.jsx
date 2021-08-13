45027bc47ada499622cf205942d0823528db9f37
Raynaldo Sutisna <raaynaldo@gmail.com>
2021-08-12 23:35:10 -0400

    feat: insert picture project

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+
 export default function Project({ title, description, image }) {
   return (
-    <div className='p-5 border shadow-sm'>
+    <div className='p-5 space-y-2 border shadow-sm'>
       <h4>{title}</h4>
-      <p>{description}</p>
+      <p className='text-xs'>{description}</p>
+      <figure className='w-full shadow-sm'>
+        <Image
+          src='/images/love4heroes.png'
+          priority
+          width={200}
+          height={100}
+          layout='responsive'
+        />
+      </figure>
     </div>
   );
 }
